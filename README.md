cd002559070071c759fd569a85338da9cfaef040
0x4B1D <ilsecco94@gmail.com>
2025-03-08 01:19:49 +0100

    init working player

---
+++ README.md
@@ -0,0 +1,89 @@
+# Spot(ify) controller app
+
+Made for 4,3' touch lcd from wondershare, ui is built for 800x480 so scaling may be a problem.
+
+You'll need to set spotipy variables into a credentials.py file into the root of the project like, this is not secure, is just for development.
+
+```
+SPOTIPY_THING = "123123123"
+```
+
+## Run the app
+
+### uv
+
+Run as a desktop app:
+
+```
+uv run flet run
+```
+
+Run as a web app:
+
+```
+uv run flet run --web
+```
+
+### Poetry
+
+Install dependencies from `pyproject.toml`:
+
+```
+poetry install
+```
+
+Run as a desktop app:
+
+```
+poetry run flet run
+```
+
+Run as a web app:
+
+```
+poetry run flet run --web
+```
+
+For more details on running the app, refer to the [Getting Started Guide](https://flet.dev/docs/getting-started/).
+
+## Build the app
+
+### Android
+
+```
+flet build apk -v
+```
+
+For more details on building and signing `.apk` or `.aab`, refer to the [Android Packaging Guide](https://flet.dev/docs/publish/android/).
+
+### iOS
+
+```
+flet build ipa -v
+```
+
+For more details on building and signing `.ipa`, refer to the [iOS Packaging Guide](https://flet.dev/docs/publish/ios/).
+
+### macOS
+
+```
+flet build macos -v
+```
+
+For more details on building macOS package, refer to the [macOS Packaging Guide](https://flet.dev/docs/publish/macos/).
+
+### Linux
+
+```
+flet build linux -v
+```
+
+For more details on building Linux package, refer to the [Linux Packaging Guide](https://flet.dev/docs/publish/linux/).
+
+### Windows
+
+```
+flet build windows -v
+```
+
+For more details on building Windows package, refer to the [Windows Packaging Guide](https://flet.dev/docs/publish/windows/).
---
+++ src/main.py
@@ -0,0 +1,367 @@
+import time
+import flet as ft
+import spotipy
+
+from spotipy.oauth2 import SpotifyOAuth
+from apscheduler.schedulers.background import BackgroundScheduler
+import credentials
+
+scope = "user-library-read, user-read-playback-state, user-modify-playback-state, user-read-currently-playing, user-read-playback-position, user-read-recently-played"
+
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        client_id=credentials.SPOTIPY_CLIENT_ID,
+        client_secret=credentials.SPOTIPY_CLIENT_SECRET,
+        redirect_uri=credentials.SPOTIPY_REDIRECT_URI,
+        scope=scope,
+    )
+)
+
+
+class SpotifyController:
+    def __init__(self):
+        devices = sp.devices()
+        for device in devices["devices"]:
+            if device["name"] == "SAMWIN":
+                self.deviceid = device["id"]
+                self.volume = device["volume_percent"]
+
+        self.playback_state = sp.current_playback()
+        self.currently_playing = sp.currently_playing()
+
+        if self.playback_state is not None:
+            self.trackimage = self.playback_state["item"]["album"]["images"][1]["url"]
+            self.trackartist = ""
+            for i in self.playback_state["item"]["artists"]:
+                self.trackartist = self.trackartist + i["name"] + ", "
+            self.trackartist = self.trackartist[0:-2]
+            self.trackname = self.playback_state["item"]["name"]
+            self.trackalbum = self.playback_state["item"]["album"]["name"]
+            self.is_playing = self.playback_state["is_playing"]
+            self.duration = self.playback_state["item"]["duration_ms"] / 1000
+            if self.currently_playing is not None:
+                self.progress = self.currently_playing["progress_ms"] / 1000
+            else:
+                self.progress = 0
+        else:
+            self.playback_state = sp.current_user_recently_played(limit=1)
+            self.trackimage = self.playback_state["items"][0]["track"]["album"][
+                "images"
+            ][1]["url"]
+            self.trackartist = ""
+            for i in self.playback_state["items"][0]["track"]["artists"]:
+                self.trackartist = self.trackartist + i["name"] + ", "
+            self.trackartist = self.trackartist[0:-2]
+            self.trackname = self.playback_state["items"][0]["track"]["name"]
+            self.trackalbum = self.playback_state["items"][0]["track"]["album"]["name"]
+            self.duration = (
+                self.playback_state["items"][0]["track"]["duration_ms"] / 1000
+            )
+            self.progress = 0
+            self.is_playing = False
+
+    def update_playback(self):
+        self.playback_state = sp.current_playback()
+        if self.playback_state is not None:
+            self.trackimage = self.playback_state["item"]["album"]["images"][1]["url"]
+            self.trackartist = ""
+            for i in self.playback_state["item"]["artists"]:
+                self.trackartist = self.trackartist + i["name"] + ", "
+            self.trackartist = self.trackartist[0:-2]
+            self.trackname = self.playback_state["item"]["name"]
+            self.trackalbum = self.playback_state["item"]["album"]["name"]
+            self.is_playing = self.playback_state["is_playing"]
+        else:
+            self.playback_state = sp.current_user_recently_played(limit=1)
+            self.trackimage = self.playback_state["items"][0]["track"]["album"][
+                "images"
+            ][1]["url"]
+            self.trackartist = ""
+            for i in self.playback_state["items"][0]["track"]["artists"]:
+                self.trackartist = self.trackartist + i["name"] + ", "
+            self.trackartist = self.trackartist[0:-2]
+            self.trackname = self.playback_state["items"][0]["track"]["name"]
+            self.trackalbum = self.playback_state["items"][0]["track"]["album"]["name"]
+            self.is_playing = False
+
+    def update_state(self):
+        if self.is_playing == True:
+            self.currently_playing = sp.currently_playing()
+            self.duration = self.currently_playing["item"]["duration_ms"] / 1000
+            self.progress = self.currently_playing["progress_ms"] / 1000
+            devices = sp.devices()
+            for device in devices["devices"]:
+                if device["name"] == "SAMWIN":
+                    self.deviceid = device["id"]
+                    self.volume = device["volume_percent"]
+
+    def next_track(self):
+        sp.next_track()
+        time.sleep(0.4)
+        self.update_playback()
+
+    def previous_track(self):
+        sp.previous_track()
+        time.sleep(0.4)
+        self.update_playback()
+
+    def seek_track(self, seek_ms):
+        sp.seek_track(seek_ms, self.deviceid)
+        time.sleep(0.4)
+        self.update_state()
+
+    def play_pause(self):
+        if self.is_playing == True:
+            sp.pause_playback(self.deviceid)
+        else:
+            sp.start_playback(self.deviceid)
+        time.sleep(0.3)
+        self.update_playback()
+
+    def update_volume(self, volume):
+        sp.volume(volume, self.deviceid)
+
+
+spc = SpotifyController()
+
+
+def main(page: ft.Page):
+    scheduler = BackgroundScheduler()
+    scheduler.start()
+    job = None
+
+    def start_refresh():
+        try:
+            scheduler.add_job(
+                func=timer_refresh,
+                trigger="interval",
+                seconds=1,
+                id="timer",
+                max_instances=1,
+            )
+
+        except Exception as e:
+            scheduler.resume_job(job_id="timer", jobstore="default")
+
+    def stop_timer():
+        try:
+            scheduler.pause_job(job_id="timer", jobstore="default")
+        except Exception as e:
+            pass
+
+    def timer_refresh():
+        spc.update_playback()
+        spc.update_state()
+        update_fields()
+        page.update()
+
+    def play_pause(e):
+        spc.play_pause()
+        update_fields()
+        if spc.is_playing == True:
+            start_refresh()
+        else:
+            stop_timer()
+        page.update()
+
+    def previous_track(e):
+        spc.previous_track()
+        update_fields()
+        page.update()
+
+    def next_track(e):
+        spc.next_track()
+        update_fields()
+        page.update()
+
+    def update_fields():
+        if spc.is_playing == True:
+            tticonbtn.icon = ft.Icons.PAUSE
+            tticonbtn.opacity = 0
+            ttimg.opacity = 1
+            ttimg.color = None
+            ttimg.color_blend_mode = ft.BlendMode.COLOR
+        else:
+            ttimg.color = ft.Colors.BLACK
+            ttimg.color_blend_mode = ft.BlendMode.SATURATION
+            ttimg.opacity = 0.5
+            tticonbtn.icon = ft.Icons.PLAY_ARROW
+            tticonbtn.opacity = 0.8
+        ttimg.src = spc.trackimage
+        tname.value = spc.trackname
+        tartist.value = "### **" + spc.trackartist + "**"
+        talbum.value = spc.trackalbum
+        ttime.content.value = int(spc.progress)
+        ttime.content.max = int(spc.duration)
+        tdur.value = time.strftime("%M:%S", time.gmtime(spc.duration))
+        t0.value = time.strftime("%M:%S", time.gmtime(spc.progress))
+        tvolume.value = spc.volume
+
+    def seek_track(e):
+        spc.seek_track(int(e.control.value * 1000))
+        ttime.content.value = e.control.value
+
+    def update_volume(e):
+        tvolume.value = e.control.value
+        spc.update_volume(int(e.control.value))
+        tvolume.update()
+
+    tvolume = ft.Slider(min=0, max=100, value=spc.volume, on_change_end=update_volume)
+
+    ttime = ft.Container(
+        content=ft.Slider(
+            value=int(spc.progress),
+            min=0,
+            max=int(spc.duration),
+            on_change_end=seek_track,
+            width=250,
+        )
+    )
+    tdur = ft.Text(
+        time.strftime("%M:%S", time.gmtime(spc.duration)),
+        size=12,
+        color=ft.Colors.WHITE,
+    )
+
+    t0 = ft.Text(
+        time.strftime("%M:%S", time.gmtime(spc.progress)),
+        size=12,
+        color=ft.Colors.WHITE,
+    )
+    ttimg = ft.Image(
+        src=spc.trackimage,
+        width=200,
+        height=200,
+        fit=ft.ImageFit.CONTAIN,
+        border_radius=ft.border_radius.all(8),
+    )
+    tticonbtn = ft.IconButton(
+        ft.Icons.PLAY_ARROW,
+        icon_size=128,
+        icon_color=ft.Colors.WHITE,
+        opacity=0,
+        on_click=play_pause,
+    )
+    timg = ft.ElevatedButton(
+        content=ft.Stack(
+            [
+                ttimg,
+                ft.Container(
+                    content=tticonbtn,
+                    alignment=ft.alignment.center,
+                    width=200,
+                    height=200,
+                    on_click=play_pause,
+                ),
+            ],
+            alignment=ft.alignment.center,
+        ),
+        on_click=play_pause,
+    )
+    tname = ft.Text(
+        spc.trackname,
+        size=24,
+        weight=ft.FontWeight.BOLD,
+        # overflow=ft.TextOverflow.ELLIPSIS,
+        text_align=ft.TextAlign.CENTER,
+        max_lines=2,
+    )
+    tartist = ft.Markdown("### **" + spc.trackartist + "**")
+    talbum = ft.Text(spc.trackalbum)
+
+    prevtrack = ft.IconButton(
+        icon=ft.Icons.SKIP_PREVIOUS_ROUNDED,
+        icon_size=64,
+        icon_color=ft.Colors.WHITE,
+        style=ft.ButtonStyle(bgcolor=ft.Colors.BLACK),
+        on_click=previous_track,
+    )
+    nexttrack = ft.IconButton(
+        icon=ft.Icons.SKIP_NEXT_ROUNDED,
+        icon_size=64,
+        icon_color=ft.Colors.WHITE,
+        style=ft.ButtonStyle(bgcolor=ft.Colors.BLACK),
+        on_click=next_track,
+    )
+    play_fab = ft.FloatingActionButton(
+        icon=ft.Icons.PLAY_ARROW if not spc.is_playing else ft.Icons.PAUSE,
+        on_click=play_pause,
+        scale=2,
+    )
+    trackinfocol = ft.Container(
+        ft.Column(
+            [
+                ft.Container(timg, alignment=ft.alignment.center),
+                ft.Container(
+                    ft.Row(
+                        [t0, ttime, tdur],
+                        alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+                    ),
+                    alignment=ft.alignment.center,
+                ),
+                ft.Container(tname),
+                ft.Container(tartist),
+                ft.Container(talbum),
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+        ),
+        alignment=ft.alignment.center,
+        expand=2,
+    )
+    prevtrackcol = ft.Container(
+        ft.Column([ft.Container(prevtrack)]),
+        alignment=ft.alignment.center,
+        expand=1,
+    )
+    nexttrackcol = ft.Container(
+        ft.Column([ft.Container(nexttrack)]),
+        alignment=ft.alignment.center,
+        expand=1,
+    )
+
+    page.views.append(
+        ft.View(
+            "/",
+            [
+                ft.Container(
+                    content=ft.Row(
+                        [
+                            prevtrackcol,
+                            trackinfocol,
+                            nexttrackcol,
+                        ],
+                        alignment=ft.MainAxisAlignment.SPACE_AROUND,
+                    ),
+                    alignment=ft.alignment.center,
+                    width=800,
+                ),
+                ft.Container(
+                    ft.Row(
+                        [ft.Container(tvolume)],
+                    ),
+                ),
+            ],
+            vertical_alignment=ft.MainAxisAlignment.CENTER,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+            spacing=2,
+        )
+    )
+
+    page.window.max_height = 480
+    page.window.max_width = 800
+    page.window.resizable = False
+    page.window.maximized = True
+    page.window.full_screen = True
+    page.window.title_bar_hidden = True
+    page.theme = ft.Theme(
+        color_scheme_seed=ft.Colors.BLUE_200,
+        page_transitions=ft.PageTransitionsTheme(windows=ft.PageTransitionTheme.NONE),
+        use_material3=True,
+    )
+    if spc.is_playing == True:
+        start_refresh()
+    page.go("/")
+
+
+ft.app(main)
